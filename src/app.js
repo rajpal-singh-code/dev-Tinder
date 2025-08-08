f66d1332c2b1956f73d0f887dbb153c46f63cc42
rajpal-singh-code <rajpalku2022@gmail.com>
2025-08-08 11:40:57 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,31 +1,24 @@
 const express = require("express");    // npm i -g nodemon (whenever i chaged the any code then it will automatically update);
 const app = express();
 
-// This will only handle GET call to /user
-app.get("/user", (req,res) => {
-    res.send({firstName: "Rajpal", lastName: "Kumar"})
-})
 
-// This will only handle POST call to /user
-app.post("/user", (req,res) => {
-    res.send("Data successfully saved to the database");
-})
-
-// This will only handle DELETE call to /user
-app.delete("/user", (req,res) => {
-    res.send("Deleted Successfully");
-})
-
-app.patch("/user", (req,res) => {
-    res.send("Data is Patched");
-})
-
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");  // ← send response to browser
-});
-
+app.use("/user",[ (req,res,next) => {
+    console.log("Handling the route user!");
+    next();
+    // res.send("Hi Bro!");
+},
+(req,res,next) => {
+    console.log("Handling the route user 2!");    
+    // res.send("2nd Brother!");
+    next();
+},
+(req,res) => {
+    console.log("Handling the route user 3!");    
+    res.send("3rd Brother!");
+}
+])
 
 app.listen(3000, () => {
+
     console.log("Server is successfully listening on port 3000...");
 });

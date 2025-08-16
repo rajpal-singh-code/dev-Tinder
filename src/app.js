386e31f9075c48492c10405087d211781ad8c613
rajpal-singh-code <rajpalku2022@gmail.com>
2025-08-16 12:28:18 +0530

    get APIS

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON body
+app.use(express.json()); 
 
 app.use("/signup", async (req, res) => {
     // Creating a new instance of the User model
@@ -16,6 +16,34 @@ app.use("/signup", async (req, res) => {
     }
 
     console.log(req.body)
+});
+
+// Get user by email
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.findOne({emailId: userEmail});
+        res.send(user);
+        // const data = await User.find({emailId: userEmail});
+        // if(data.length === 0)
+        //     res.status(404).send("User not found");
+        // else
+        //     res.send(data);
+    }
+    catch(err) {
+        res.status(400).send("Something went wrong ", + err.message);
+    }
+});
+
+// Feed API - GET/feed -get all the users from the database
+app.get("/feed",async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
 });
 
 connectDB()

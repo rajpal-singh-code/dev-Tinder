d5fff13139961dabfab907e42ec5c974c361b288
rajpal-singh-code <rajpalku2022@gmail.com>
2025-08-17 22:58:21 +0530

    Delete and Update API

--- src/app.js
+++ src/app.js
@@ -5,46 +5,32 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json()); 
 
-app.use("/signup", async (req, res) => {
-    // Creating a new instance of the User model
-    const user = new User(req.body);
-    try {
-        await user.save();
-        res.send("User Added successfully");
+
+
+// Delete a user from database
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete({_id: userId});
+        // const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
     } catch (err) {
-        res.status(500).send("Error saving the user: " + err.message);
-    }
-
-    console.log(req.body)
-});
-
-// Get user by email
-app.get("/user", async (req,res) => {
-    const userEmail = req.body.emailId;
-    try{
-        const user = await User.findOne({emailId: userEmail});
-        res.send(user);
-        // const data = await User.find({emailId: userEmail});
-        // if(data.length === 0)
-        //     res.status(404).send("User not found");
-        // else
-        //     res.send(data);
-    }
-    catch(err) {
-        res.status(400).send("Something went wrong ", + err.message);
-    }
-});
-
-// Feed API - GET/feed -get all the users from the database
-app.get("/feed",async (req,res) => {
-    try{
-        const users = await User.find({});
-        res.send(users);
-    }
-    catch(err){
         res.status(400).send("Something went wrong");
     }
 });
+
+// Update data of the user
+app.patch("/user", async (req,res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    console.log(data);
+    try{
+        await User.findByIdAndUpdate({_id: userId},data);
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send("something went wrong");
+    }
+})
 
 connectDB()
     .then(() => {

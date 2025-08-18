773ff4f415ed9b097e974a26e250b9305f38ce6f
rajpal-singh-code <rajpalku2022@gmail.com>
2025-08-18 11:41:00 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -5,7 +5,46 @@ const User = require("./models/user");
 const app = express();
 app.use(express.json()); 
 
+app.use("/signup", async (req, res) => {
+    // Creating a new instance of the User model
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("User Added successfully");
+    } catch (err) {
+        res.status(500).send("Error saving the user: " + err.message);
+    }
 
+    console.log(req.body)
+});
+
+// Get user by email
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.findOne({emailId: userEmail});
+        res.send(user);
+        // const data = await User.find({emailId: userEmail});
+        // if(data.length === 0)
+        //     res.status(404).send("User not found");
+        // else
+        //     res.send(data);
+    }
+    catch(err) {
+        res.status(400).send("Something went wrong ", + err.message);
+    }
+});
+
+// Feed API - GET/feed -get all the users from the database
+app.get("/feed",async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
 
 // Delete a user from database
 app.delete("/user", async (req,res) => {

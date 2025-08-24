beef7c3a9128637b647d758fe1dea6a75eed8011
rajpal-singh-code <rajpalku2022@gmail.com>
2025-08-25 01:16:44 +0530

    Created login API

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 const express = require("express");
+const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
-const app = express();
 app.use(express.json()); 
 
-app.use("/signup", async (req, res) => {
-    // Creating a new instance of the User model
-    const user = new User(req.body);
+app.post("/signup", async (req, res) => {
     try {
+        // validate of data
+        validateSignUpData(req);
+
+        const { firstName,lastName,emailId,password } = req.body;
+        // Encrypt the password
+        const passwordHash = await bcrypt.hash(password,10);
+        console.log(passwordHash);
+
+        // Creating a new instance of the User model
+        const user = new User({
+            firstName,lastName,emailId,password:passwordHash,
+        });
         await user.save();
         res.send("User Added successfully");
     } catch (err) {
-        res.status(500).send("Error saving the user: " + err.message);
+        res.status(400).send("ERROR : " + err.message);
     }
 
-    console.log(req.body)
 });
 
 // Get user by email
@@ -34,6 +45,21 @@ app.get("/user", async (req,res) => {
         res.status(400).send("Something went wrong ", + err.message);
     }
 });
+
+// Login Api
+app.post("/login", async(req,res) => {
+    try{
+        const { emailId,password } = req.body;
+        
+        const user = await User.findOne({ emailId: emailId});
+        if(!user) throw new Error("Invalid credentials");
+        const isPassword = await bcrypt.compare(password,user.password);
+        if(!isPassword) throw new Error("Invalid credentials");
+        else res.status(200).send("Login Sucessfully");
+    } catch (err){
+        res.status(404).send("ERROR : "+ err.message);
+    }
+})
 
 // Feed API - GET/feed -get all the users from the database
 app.get("/feed",async (req,res) => {
@@ -59,31 +85,45 @@ app.delete("/user", async (req,res) => {
 });
 
 // Update data of the user
-// app.patch("/user", async (req,res) => {
-//     const userId = req.body.Id;
-//     const data = req.body;
-//     console.log(data);
-//     try{
-//         await User.findByIdAndUpdate({_id: userId},data);
-//         res.send("User updated successfully");
-//     } catch (err) {
-//         res.status(400).send("something went wrong");
-//     }
-// })
-
-app.patch("/user", async(req,res) => {
-    const emailId = req.body.emailId;
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params?.userId;
     const data = req.body;
-    try{
-        await User.findOneAndUpdate({emailId: emailId},data, {
-            runValidators: true,
+    
+    try {
+        const ALLOWED_UPDATED = ["age","about","skills","gender","photoUrl","firstName","lastName"];
+        const isUpdatedAllowed = Object.keys(data).every((k) =>
+            k === "userId" || ALLOWED_UPDATED.includes(k)
+        );
+        if(!isUpdatedAllowed){
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length>10){
+            throw new Error("cannot add more than 10 skill")
+        }
+        const user = await User.findByIdAndUpdate({ _id: userId }, data ,{
             returnDocument: "after",
+            runValidators: true,
         });
-        res.send("User Updated successfully");
-    } catch(err) {
-        res.status(400).send("UPDATED FAILED :" + err.message);
+        console.log(user);
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send(err.message + ": something went wrong");
     }
 })
+
+// app.patch("/user", async(req,res) => {
+//     const emailId = req.body.emailId;
+//     const data = req.body;
+//     try{
+//         await User.findOneAndUpdate({emailId: emailId},data, {
+//             runValidators: true,
+//             returnDocument: "after",
+//         });
+//         res.send("User Updated successfully");
+//     } catch(err) {
+//         res.status(400).send("UPDATED FAILED :" + err.message);
+//     }
+// })
 
 connectDB()
     .then(() => {
@@ -97,4 +137,6 @@ connectDB()
     });
 
 
-    ///hello
+
+    // if you want to use validator then install : npm install validator
+    // if you want to password encrypt then install : npm install bcrypt
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -16,10 +17,20 @@ const UserSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)) {
+                throw new Error("Invalid email address: "+ value);
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)) {
+                throw new Error("this is not strong password : "+ value);
+            }
+        }
     },
     age: {
         type: Number
@@ -37,7 +48,12 @@ const UserSchema = new mongoose.Schema({
     },
     photoUrl: {
         type: String,
-        default: "https://www.shutterstock.com/image-photo/blond-hair-girl-taking-photo-260nw-2492842415.jpg"
+        default: "https://www.shutterstock.com/image-photo/blond-hair-girl-taking-photo-260nw-2492842415.jpg",
+        validate(value){
+            if(!validator.isURL(value)) {
+                throw new Error("Invalid URL address: "+ value);
+            }
+        }
     },
     about: {
         type: String,
